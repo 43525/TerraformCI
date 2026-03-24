353559415dec5a37d2431e5af461c3297738723b
43525 <alvin43525@gmail.com>
2026-03-24 07:40:41 +0000

    rm rg

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
-  subscription_id  = "9734ed68-621d-47ed-babd-269110dbacb1"
+  subscription_id                 = "9734ed68-621d-47ed-babd-269110dbacb1"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagemyvm1"
+  name                     = "storagemyvm2"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
