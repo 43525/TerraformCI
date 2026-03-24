d4c74b8f4e11c306a4e894a4add6cf996318fd9b
Alvin <107040529+43525@users.noreply.github.com>
2026-03-24 15:02:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,21 +15,4 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  resource_provider_registrations = "none"
-  subscription_id  = "9734ed68-621d-47ed-babd-269110dbacb1"
-}
 
-resource "azurerm_resource_group" "rg" {
-  name     = "1-b6ec31fc-playground-sandbox"
-  location = "eastus"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "storagemyvm1"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
